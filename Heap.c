3b5a3500c482891fe8e44e66d946a876f2a40e00
paniz-y <paniz84yaghoobzadeh@gmail.com>
2025-12-13 00:42:28 +0330

    Complete hinit function

--- Heap.c
+++ Heap.c
@@ -1,14 +1,24 @@
 #include "Chunk.h"
 #include "Heap.h"
 
-int hinit(size_t heapSize)
+
+int hinit(size_t heapSize, struct heap_t* heap)
 {
     if(!heapSize)
     {
         errno = EINVAL;
         return -1;
     }
-    allignPage(heapSize);
+    size_t allignedPageSize = allignPage(heapSize);
+    struct chunk_t* firstChunk = mmap(NULL, allignedPageSize, PROT_READ | PROT_WRITE, MAP_PRIVATE | MAP_ANONYMOUS, -1, 0);
+    size_t remainedAvail = allignedPageSize - sizeof(struct chunk_t);
+    firstChunk->size = remainedAvail;
+    firstChunk->inuse = 0;
+    firstChunk->next = NULL;
+
+    heap->start = firstChunk;
+    heap->avail = firstChunk->size;
+
     return 0;
 }
 size_t allignPage(size_t heapSize)
@@ -16,4 +26,12 @@ size_t allignPage(size_t heapSize)
     long pageSize = sysconf(_SC_PAGESIZE);
     return ((heapSize + pageSize - 1) / pageSize) * pageSize;
     
+}
+
+int main()
+{
+    struct heap_t heap;
+    hinit(4096, &heap);
+    printf("%p %d", (void*)heap.start, heap.avail);
+    return 0;
 }
--- Heap.h
+++ Heap.h
@@ -3,12 +3,14 @@
 #include <stdint.h>
 #include <stddef.h>
 #include <errno.h>
-#include <bits/confname.h>
+#include <unistd.h>
+#include <sys/mman.h>
+#include <stdio.h>
 struct heap_t 
 {
 struct chunk_t *start;
 uint32_t avail; // available memory
 };
-int hinit(const size_t heapSize);
+int hinit(const size_t heapSize ,struct heap_t* heap);
 size_t allignPage(const size_t heapSize);
 #endif
