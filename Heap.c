63a2c99e6c95b259e9df4586debb05aeb23adfca
paniz-y <paniz84yaghoobzadeh@gmail.com>
2025-12-13 22:41:37 +0330

    Add first fit function

--- Heap.c
+++ Heap.c
@@ -1,17 +1,16 @@
 #include "Chunk.h"
 #include "Heap.h"
 
-
-int hinit(size_t heapSize, struct heap_t* heap)
+int hinit(size_t heapSize, struct heap_t *heap)
 {
-    if(!heapSize)
+    if (!heapSize)
     {
         errno = EINVAL;
         return -1;
     }
-    size_t allignedPageSize = allignPage(heapSize);
-    struct chunk_t* firstChunk = mmap(NULL, allignedPageSize, PROT_READ | PROT_WRITE, MAP_PRIVATE | MAP_ANONYMOUS, -1, 0);
-    size_t remainedAvail = allignedPageSize - sizeof(struct chunk_t);
+    size_t alignedPageSize = allignPage(heapSize);
+    struct chunk_t *firstChunk = mmap(NULL, alignedPageSize, PROT_READ | PROT_WRITE, MAP_PRIVATE | MAP_ANONYMOUS, -1, 0);
+    size_t remainedAvail = alignedPageSize - sizeof(struct chunk_t);
     firstChunk->size = remainedAvail;
     firstChunk->inuse = 0;
     firstChunk->next = NULL;
@@ -21,17 +20,41 @@ int hinit(size_t heapSize, struct heap_t* heap)
 
     return 0;
 }
-size_t allignPage(size_t heapSize)
+size_t alignPage(size_t heapSize)
 {
     long pageSize = sysconf(_SC_PAGESIZE);
     return ((heapSize + pageSize - 1) / pageSize) * pageSize;
-    
 }
-
+size_t align8(size_t size)
+{
+    return ((size + 8 - 1) / 8) * size;
+}
+void *halloc(const size_t size)
+{
+    if (!size)
+    {
+        errno = EINVAL;
+        return -1;
+    }
+    size_t alignedSize = align8(size);
+}
+struct chunk_t *firstFit(const size_t size, struct heap_t *heap)
+{
+    struct chunk_t *currChuck = heap->start;
+    while (currChuck)
+    {
+        if (!currChuck->inuse && size <= currChuck->size)
+        {
+            return currChuck;
+        }
+        currChuck = currChuck->next;
+        return NULL;
+    }
+}
 int main()
 {
     struct heap_t heap;
     hinit(4096, &heap);
-    printf("%p %d", (void*)heap.start, heap.avail);
+    printf("%p %d", (void *)heap.start, heap.avail);
     return 0;
 }
--- Heap.h
+++ Heap.h
@@ -12,5 +12,9 @@ struct chunk_t *start;
 uint32_t avail; // available memory
 };
 int hinit(const size_t heapSize ,struct heap_t* heap);
-size_t allignPage(const size_t heapSize);
+size_t alignPage(const size_t heapSize);
+size_t align8(size_t size);
+void *halloc(const size_t size);
+struct chunk_t *firstFit(const size_t size, struct heap_t *heap);
+
 #endif
