93b77e4c873dc58890287b7e77bab6111c4ac9e6
marziehShooshtarii <shooshtarimarzieh83@gmail.com>
2025-12-15 08:27:54 +0330

    heap boundry checking added

--- Heap.h
+++ Heap.h
@@ -8,14 +8,16 @@
 #include <stdio.h>
 struct heap_t 
 {
-struct chunk_t *start;
+struct chunk_t *start; // the chunk list head
 uint32_t avail; // available memory
+void *heap_start; //used for bounbry
+void *heap_end;
 };
 int hinit(const size_t heapSize ,struct heap_t* heap);
 size_t alignPage(const size_t heapSize);
 size_t align8(size_t size);
 void *halloc(const size_t size, struct heap_t *heap);
-struct chunk_t *firstFit(const size_t size, struct heap_t *heap, struct chunk_t *prevChunk);
+struct chunk_t *firstFit(const size_t size, struct heap_t *heap, struct chunk_t **prevChunk);
 struct chunk_t *requestMemory(size_t size, struct heap_t *heap);
 void hfree(void *ptr, struct heap_t *heap);
 
