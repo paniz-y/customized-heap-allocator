a00e13166e2dbc41c4d64555f9f86ffe434fae7d
marziehShooshtarii <shooshtarimarzieh83@gmail.com>
2025-12-15 08:27:09 +0330

    magic number addded

--- Chunk.h
+++ Chunk.h
@@ -1,10 +1,12 @@
 #ifndef CHUNK_H
 #define CHUNK_H
 #include <stdint.h>
+#define CHUNK_MAGIC 0xDEADBEEF
 struct chunk_t
 {
     uint32_t size;
     uint8_t inuse; // boolean
     struct chunk_t *next;
+    uint32_t magic;
 };
 #endif
