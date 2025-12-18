a350ca5d1f42c27103cdb1fa55e6f52d6a2fe7a4
marziehShooshtarii <shooshtarimarzieh83@gmail.com>
2025-12-18 19:22:46 +0330

    Add region struct

--- Heap.h
+++ Heap.h
@@ -6,21 +6,27 @@
 #include <unistd.h>
 #include <sys/mman.h>
 #include <stdio.h>
-struct heap_t 
+struct heap_t
 {
-struct chunk_t *start; // the chunk list head
-uint32_t avail; // available memory
-void *heap_start; //used for bounbry
-void *heap_end;
+    struct chunk_t *start; // the chunk list head
+    uint32_t avail;        // available memory
+    struct region_t *regions;
 };
-int hinit(const size_t heapSize ,struct heap_t* heap);
+struct region_t
+{
+    void *startOfRegion; // start of the region
+    void *endOfRegion;   // end of the reaion for boundry check
+    struct region_t *next;
+};
+
+int hinit(const size_t heapSize, struct heap_t *heap);
 size_t alignPage(const size_t heapSize);
 size_t align8(size_t size);
 void *halloc(const size_t size, struct heap_t *heap);
 struct chunk_t *firstFit(const size_t size, struct heap_t *heap, struct chunk_t **prevChunk);
 struct chunk_t *requestMemory(size_t size, struct heap_t *heap);
+int exictingPtrInHeap(void *ptr, struct heap_t *heap);
 void hfree(void *ptr, struct heap_t *heap);
 void coalescing(struct heap_t *heap);
-
 
 #endif
