c15d08b114b75606380f6ab143917761065cdcf0
marziehShooshtarii <shooshtarimarzieh83@gmail.com>
2025-12-21 10:34:08 +0330

    Add pool.c

---
+++ Pool.c
@@ -0,0 +1,66 @@
+#include "Pool.h"
+#include "Heap.h"
+#include <sys/mman.h>
+#include <stdint.h>
+
+void poolInit(struct heap_t *heap)
+{
+    heap->pools = NULL;
+
+    for (size_t i = 0; i < heap->numPools; i++)
+    {
+        struct pool_t *firstPool = mmap(NULL, sizeof(struct pool_t), PROT_READ | PROT_WRITE, MAP_PRIVATE | MAP_ANONYMOUS, -1, 0);
+
+        firstPool->blockSize = heap->poolSizes[i];
+        firstPool->freeList = NULL;
+        firstPool->next = heap->pools;
+
+        heap->pools = firstPool;
+    }
+}
+
+void *poolAlloc(size_t size, struct heap_t *heap)
+{
+    struct pool_t *newPool = heap->pools;
+
+    while (newPool && newPool->blockSize < size) // find the smallest pool fiting the size
+        newPool = newPool->next;
+
+    if (!newPool)
+        return NULL;
+
+    void *poolPage = mmap(NULL, POOL_PAGE_SIZE, PROT_READ | PROT_WRITE, MAP_PRIVATE | MAP_ANONYMOUS, -1, 0);
+
+    size_t numberOfBlocks = POOL_PAGE_SIZE / newPool->blockSize;
+
+    char *pagePtr = (char *)poolPage;
+
+    for (size_t i = 0; i < numberOfBlocks; i++)
+    {
+        *(void **)pagePtr = newPool->freeList;
+        newPool->freeList = pagePtr;
+        pagePtr += newPool->blockSize;
+    }
+
+    void *newBlock = newPool->freeList;
+    newPool->freeList = *(void **)newBlock;
+
+    return newBlock;
+}
+
+int poolFree(void *ptr, struct heap_t *heap)
+{
+    struct pool_t *pool = heap->pools;
+
+    while (pool)
+    {
+        if (((uintptr_t)ptr % pool->blockSize) == 0)
+        {
+            *(void **)ptr = pool->freeList;
+            pool->freeList = ptr;
+            return 1;
+        }
+        pool = pool->next;
+    }
+    return 0;
+}
